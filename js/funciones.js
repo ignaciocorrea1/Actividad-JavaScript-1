aa6027c69e32abe6413289c55490eb024f04ccac
ignacioc <ig.correar@duocuc>
2024-04-23 11:59:28 -0400

    Actividad
    Actividad validacion de formulario

---
+++ js/funciones.js
@@ -0,0 +1,6 @@
+function validar(){
+    // Variables
+    let nombre = document.getElementById("nombre").ariaValueMax;
+    let rut = document.getElementById("rut").ariaValueMax;
+    let correo = document.getElementById("correo").ariaValueMax;   
+}
