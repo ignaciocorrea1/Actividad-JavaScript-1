0db5f26b7237c9b38f24b0d65f0234da3551cf64
ignacioc <ig.correar@duocuc.cl>
2024-04-26 11:55:53 -0400

    Agregado de JQuery
    Agregado de JQuery

--- js/funciones.js
+++ js/funciones.js
@@ -1,6 +1,20 @@
-function validar(){
-    // Variables
-    let nombre = document.getElementById("nombre").ariaValueMax;
-    let rut = document.getElementById("rut").ariaValueMax;
-    let correo = document.getElementById("correo").ariaValueMax;   
+function validar() {
+  // Variables
+  let nombre = document.getElementById("nombre").value;
+  let rut = document.getElementById("rut").value;
+  let correo = document.getElementById("correo").value;
+
+  /*
+              Nombre Min: 15 - Max: 25
+              Rut Min: 9 - Max: 12
+              Correo Min: 5 - Max: 25 - Debe contener gmail || outlook || live 
+          */
+
+  // Validar Nombre completo
+  if (String(nombre).length >= 15 && String(nombre).length <= 25) {
+    $("#nombre").css("border", "3px solid green");
+  }
+  else {
+    $("#nombre").css("border", "3px solid red");
+  }
 }
